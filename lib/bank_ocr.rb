d3f15b4217b6020455bc9d01d89a2314547fa6d9
Chris Magnacca <chrismagnacca@gmail.com>
2013-01-31 22:29:35 -0500

    removed unecessary code

--- lib/bank_ocr.rb
+++ lib/bank_ocr.rb
@@ -1,5 +1,4 @@
 module BankOcr
-
   require File.expand_path(File.dirname(__FILE__) + "/bank_ocr/parser")
   require File.expand_path(File.dirname(__FILE__) + "/bank_ocr/checksum")
   require File.expand_path(File.dirname(__FILE__) + "/bank_ocr/program")
