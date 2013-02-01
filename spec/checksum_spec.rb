c6620ab1a0af373b1756e385c4670fed1103c33a
Chris Magnacca <chrismagnacca@gmail.com>
2013-01-31 22:23:40 -0500

    added additional specs to validate file output

--- spec/checksum_spec.rb
+++ spec/checksum_spec.rb
@@ -18,9 +18,25 @@ describe BankOcr::Checksum do
   it %q{should output ERR for an invalid account number 457508001} do
     BankOcr.read('user_story_3.txt')
     file = File.open(File.expand_path(File.dirname(__FILE__) + "/../output/user_story_3_output.txt"))
-    file.each do |line|
-      
-    end
+    file.each { |line| line.include?('ERR').should be_true if line.include?('457508001') }
+  end
+
+  it %q{should output ILL for an invalid account number 4575080?1} do
+    BankOcr.read('user_story_3.txt')
+    file = File.open(File.expand_path(File.dirname(__FILE__) + "/../output/user_story_3_output.txt"))
+    file.each { |line| line.include?('ILL').should be_true if line.include?('4575080?1') }
+  end
+
+  it %q{should not output ILL for an valid account number 000000051} do
+    BankOcr.read('user_story_3.txt')
+    file = File.open(File.expand_path(File.dirname(__FILE__) + "/../output/user_story_3_output.txt"))
+    file.each { |line| line.include?('ILL').should be_false if line.include?('000000051') }
+  end
+
+  it %q{should not output ERR for an valid account number 000000051} do
+    BankOcr.read('user_story_3.txt')
+    file = File.open(File.expand_path(File.dirname(__FILE__) + "/../output/user_story_3_output.txt"))
+    file.each { |line| line.include?('ERR').should be_false if line.include?('000000051') }
   end
 
 end
